5ab25c2e977041f2af7efa846035be8f631b288a
ninochv <nino.chevrier@gmail.com>
2026-06-01 23:41:22 +0200

    update admin

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <SiteHeader />
+  <SiteHeader v-if="route.name !== 'admin'" />
   <main>
     <RouterView />
   </main>
-  <SiteFooter />
+  <SiteFooter v-if="route.name !== 'admin'" />
 </template>
 
 <script setup lang="ts">
+import { useRoute } from 'vue-router'
 import SiteHeader from '@/components/layout/SiteHeader.vue'
 import SiteFooter from '@/components/layout/SiteFooter.vue'
+
+const route = useRoute()
 </script>
